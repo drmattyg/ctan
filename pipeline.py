1864ec7b8fc42de54915a8671e7100843753d7f2
Matthew Gordon <inverarity@gmail.com>
2020-08-15 14:19:47 -0700

    career score

--- pipeline.py
+++ pipeline.py
@@ -19,14 +19,21 @@ def interest_overlap_score(mentee: Candidate, mentor: Candidate):
     mr = set([x.strip() for x in mentor.workin.split(',')])
     return len(me.intersection(mr))
 
+def career_score(me: Candidate, mr: Candidate):
+    categories = ["", "Less than a year", "One to three years", "Longer than three years"]
+    mr_long = categories.index(mr.howlong)
+    me_long = categories.index(me.howlong)
+    return mr_long - me_long
+
 with open(INPUT, 'r') as f:
     candidates = set([Candidate(**v) for v in DictReader(f)])
     mentees = set([c for c in candidates if c.mentormentee in ['Being mentored', 'Both']])
     mentors = candidates - mentees
-    # me = list(mentees)[0]
-    # mr = list(mentors)[0]
-    # print(me.interestedin)
-    # print(mr.workin)
-    # print(interest_overlap_score(me, mr))
+
+    me = list(mentees)[0]
+    mr = list(mentors)[0]
+    print(me.howlong)
+    print(mr.howlong)
+    print(career_score(me, mr))
 
 
