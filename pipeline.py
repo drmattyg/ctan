fba1631719d7e0cd00818ac60b1e3454f13c4eec
Matthew Gordon <inverarity@gmail.com>
2020-08-15 13:35:17 -0700

    Checkpoint

--- pipeline.py
+++ pipeline.py
@@ -4,5 +4,7 @@ INPUT = '/Users/mgordon/ctan/mentorship_july_2020/july_2020.csv'
 HEADER = ["a","firstname","lastname","email","linkedin","city","role","gender","pronouns","ethnicity","disability","accomodations","mentormentee","howmanyhours","howlong","workin","expertise","networking","interestedin","why","notes","submitted","token"]
 Candidate = namedtuple('candidate', HEADER)
 with open(INPUT, 'r') as f:
-    candidates = [Candidate(**v) for v in DictReader(f)]
-    print(candidates[0])
+    candidates = set([Candidate(**v) for v in DictReader(f)])
+    mentees = set([c for c in candidates if c.mentormentee in ['Being mentored', 'Both']])
+    mentors = candidates - mentees
+
